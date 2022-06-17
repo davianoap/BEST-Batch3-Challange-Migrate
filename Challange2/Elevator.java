1fa90c56556e5027be2ea7cba17d4fa327ac5342
Daviano Anugrah .P <daviano123@gmail.com>
2022-06-17 09:21:01 +0700

    Update Variable name

--- Challange2/Elevator.java
+++ Challange2/Elevator.java
@@ -3,26 +3,26 @@ import java.util.Scanner;
 public class Elevator {
     public static void main(String[] args) {
         Scanner sc=new Scanner(System.in);
-        int l1=1;
+        int floor=1;
         do
         {
             System.out.println("Masukkan Lantai Tujuan");
-            int l2=sc.nextInt();
+            int inputFloor=sc.nextInt();
             int i = 1;
-            if (l2 == -1) {
+            if (inputFloor == -1 || inputFloor > 30) {
                 System.exit(0);
             }
-            if(l2>l1) {
-                for( i=l1;i<l2;i++){
+            if(inputFloor>floor) {
+                for( i=floor;i<inputFloor;i++){
                     System.out.println("Elevator di lantai "+i) ;
                 }
             } else {
-                for( i=l1;i>l2;i--){
+                for( i=floor;i>inputFloor;i--){
                     System.out.println("Elevator di lantai "+i);
                 }
             }
             System.out.println("Elevator berhenti di lantai "+i);
-            l1=l2;
+            floor=inputFloor;
         }while(true);
     }
 }
